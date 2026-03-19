c452faf70afee6be7f96b1856f48eba5ab3d1453
Maximus Rosa <maximusrosadonascimento@gmail.com>
2026-03-19 17:18:39 -0300

    script do database

---
+++ scriptMysql8.4.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE escola;
+CREATE TABLE aluno(
+id INT PRIMARY KEY NOT NULL,
+nome VARCHAR(80) NOT NULL,
+email VARCHAR(120) UNIQUE NOT NULL,
+curso VARCHAR(120) NOT NULL);
+
+INSERT INTO aluno (nome,email,curso) VALUES ('Maximus Rosa','exemplo1@gmail.com','ciencia da computacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Ana Souza','exemplo2@gmail.com','engenharia de software');
+INSERT INTO aluno (nome,email,curso) VALUES ('Bruno Lima','exemplo3@gmail.com','sistemas de informacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Carla Mendes','exemplo4@gmail.com','ciencia da computacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Diego Alves','exemplo5@gmail.com','engenharia da computacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Eduarda Rocha','exemplo6@gmail.com','analise e desenvolvimento de sistemas');
+INSERT INTO aluno (nome,email,curso) VALUES ('Felipe Santos','exemplo7@gmail.com','ciencia da computacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Gabriela Costa','exemplo8@gmail.com','engenharia de software');
+INSERT INTO aluno (nome,email,curso) VALUES ('Henrique Oliveira','exemplo9@gmail.com','sistemas de informacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Isabela Pereira','exemplo10@gmail.com','ciencia da computacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Joao Martins','exemplo11@gmail.com','engenharia da computacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Karina Fernandes','exemplo12@gmail.com','analise e desenvolvimento de sistemas');
+INSERT INTO aluno (nome,email,curso) VALUES ('Lucas Ribeiro','exemplo13@gmail.com','ciencia da computacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Mariana Carvalho','exemplo14@gmail.com','engenharia de software');
+INSERT INTO aluno (nome,email,curso) VALUES ('Nicolas Barbosa','exemplo15@gmail.com','sistemas de informacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Olivia Freitas','exemplo16@gmail.com','ciencia da computacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Paulo Teixeira','exemplo17@gmail.com','engenharia da computacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Queila Araujo','exemplo18@gmail.com','analise e desenvolvimento de sistemas');
+INSERT INTO aluno (nome,email,curso) VALUES ('Rafael Gomes','exemplo19@gmail.com','ciencia da computacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Sofia Nunes','exemplo20@gmail.com','engenharia de software');
+INSERT INTO aluno (nome,email,curso) VALUES ('Tiago Batista','exemplo21@gmail.com','sistemas de informacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Ursula Duarte','exemplo22@gmail.com','ciencia da computacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Victor Andrade','exemplo23@gmail.com','engenharia da computacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Wesley Farias','exemplo24@gmail.com','analise e desenvolvimento de sistemas');
+INSERT INTO aluno (nome,email,curso) VALUES ('Xavier Moura','exemplo25@gmail.com','ciencia da computacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Yasmin Lopes','exemplo26@gmail.com','engenharia de software');
+INSERT INTO aluno (nome,email,curso) VALUES ('Zeca Monteiro','exemplo27@gmail.com','sistemas de informacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Aline Barros','exemplo28@gmail.com','ciencia da computacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Breno Castro','exemplo29@gmail.com','engenharia da computacao');
+INSERT INTO aluno (nome,email,curso) VALUES ('Camila Torres','exemplo30@gmail.com','analise e desenvolvimento de sistemas');
